a481dfc39cd4d54ec5f7ded5f79b8baf9f9f070a
melissayan <melyan@pdx.edu>
2017-06-06 12:41:40 -0700

    Initial commit

---
+++ src/com/company/main/Addition.java
@@ -0,0 +1,14 @@
+package com.company.main;
+
+/**
+ * Created by Melissa on 6/6/2017.
+ */
+public class Addition {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    public double add(double a, double b){
+        return a + b;
+    }
+}
---
+++ src/com/company/test/AdditionTest.java
@@ -0,0 +1,21 @@
+package com.company.test;
+
+import com.company.main.Addition;
+import org.junit.Test;
+
+/**
+ * Created by Melissa on 6/6/2017.
+ */
+public class AdditionTest {
+
+    @Test
+    public void test(){
+        Addition addition = new Addition();
+        double a = 2.0;
+        double b = 3.0;
+        double result = addition.add(a, b);
+        System.out.println(a + " + " + b + " = " + result);
+    }
+
+
+}
