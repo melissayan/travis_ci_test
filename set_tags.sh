e805198bb35d94c9c2c9f8878019c01db90bd672
melissayan <melyan@pdx.edu>
2017-06-09 16:30:01 -0700

    add bash script

---
+++ set_tags.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BRANCH="travis-branch"
+
+if [ "$TRAVIS_BRANCH" == "$BRANCH" ]; then
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
+  git push origin --tags
+fi
