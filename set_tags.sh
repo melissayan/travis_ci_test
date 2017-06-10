81b5ce7e593407b58b3aa5284ccab8015cc9c89e
melissayan <melyan@pdx.edu>
2017-06-09 17:46:18 -0700

    try origin

--- set_tags.sh
+++ set_tags.sh
@@ -8,5 +8,6 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
-  git push travis-branch --tags
+  git push origin --tags
+  git fetch origin
 fi
