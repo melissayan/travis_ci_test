92365b3bcc0ad56dedd75c4f72d5a15527dc747f
melissayan <melyan@pdx.edu>
2017-06-09 17:40:16 -0700

    wrong operator === used instead of =

--- set_tags.sh
+++ set_tags.sh
@@ -4,7 +4,7 @@ BRANCH="travis-branch"
 
 echo $TRAVIS_BRANCH
 
-if [ "$TRAVIS_BRANCH" == "$BRANCH" ]; then
+if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
