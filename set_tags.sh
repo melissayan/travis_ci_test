a484850d443abe4559ef5e80139398ec85199d0f
melissayan <melyan@pdx.edu>
2017-06-12 12:16:22 -0700

    release tag commit message for branch not master

--- set_tags.sh
+++ set_tags.sh
@@ -7,7 +7,8 @@ echo "TRAVIS_BRANCH: $TRAVIS_BRANCH BRANCH: $BRANCH"
 if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
+  git tag -a VariantQC Alpha v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
+  git commit -m "branch update message"
   git push origin --tags
   git fetch origin
 fi
